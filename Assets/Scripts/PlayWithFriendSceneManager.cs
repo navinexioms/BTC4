dd9c31bc04b7945ea5fc841cf0d4ffd94084d679
Navin Dhuttargi <46275302+navinexioms@users.noreply.github.com>
2019-02-25 15:38:59 +0530

    New Version of LudoBTC
    New Version of LudoBTC

---
+++ Assets/Scripts/PlayWithFriendSceneManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon;
+namespace Photon.Pun.UtilityScripts
+{
+	public class PlayWithFriendSceneManager : Photon.Pun.MonoBehaviourPun
+	{
+
+		public Toggle TwoPlayerToggle, FourPlayerToggle;
+		public GameObject TwoPlayerGameObject, FourPlayerGameObject;
+		public TPPWFConnectionManager TPPWFconnectionManager;
+		public void SelectTwoPlayerGamePlay()
+		{
+			TPPWFconnectionManager.PlayerLength = 2;
+			TPPWFconnectionManager.isSelectedGameType = true;
+			TwoPlayerGameObject.SetActive (TwoPlayerToggle.isOn);
+			FourPlayerGameObject.SetActive (false);
+		}
+		public void SelectFourPlayerGamePlay()
+		{
+			TPPWFconnectionManager.PlayerLength = 4;
+			TPPWFconnectionManager.isSelectedGameType = true;
+			TwoPlayerGameObject.SetActive (false);
+			FourPlayerGameObject.SetActive (FourPlayerToggle.isOn);
+		}
+		public void TakeRoomNameForTwoPlayers()
+		{
+			
+		}
+		public void TakeRoomNameForFourPlayers()
+		{
+			
+		}
+
+		//==============Method to Invite friend and Create Room======================//
+		public void InviteFriend()
+		{
+			
+		}
+
+		//=============Method to Join Game=================//
+		public void JoinGame()
+		{
+			
+		}
+		// Use this for initialization
+		void Start () 
+		{
+		
+		}
+	}
+}
---
+++ Assets/Scripts/TPPWFConnectionManager.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using VoxelBusters;
+using VoxelBusters.NativePlugins;
+using SimpleJSON;
+using UnityEngine.Networking;
+
+namespace Photon.Pun.UtilityScripts
+{
+	public class TPPWFConnectionManager : MonoBehaviourPunCallbacks {
+		public static bool isMaster,isRemote,JoinedRoomFlag;
+		private GameObject QuitPanel;
+		public Text RoomName,WarningText;
+		public static int PlayMode = 0;
+		public bool isSelectedGameType;
+		public int PlayerLength=0;
+		void Awake()
+		{
+			DontDestroyOnLoad (this);
+		}
+
+		public void CreateOrJoinRoomMethod()
+		{
+			if (Application.internetReachability == NetworkReachability.NotReachable) {
+				print ("no Internet connection is there");
+				StartCoroutine (RoomNameWarning ("PLEASE CONNECT TO INTERNET"));
+			} else if (!isSelectedGameType) {
+				StartCoroutine (RoomNameWarning ("PLEASE SELECT THE GAMETYPE"));
+			}else if (RoomName.text.Length > 0 && (Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork || Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork)) {
+				if (PhotonNetwork.AuthValues == null) {
+					PhotonNetwork.AuthValues = new Photon.Realtime.AuthenticationValues ();
+				}
+				string PlayerName = PlayerPrefs.GetString ("userid");
+				PhotonNetwork.AuthValues.UserId = PlayerName;
+				PhotonNetwork.LocalPlayer.NickName = PlayerName;
+				PhotonNetwork.ConnectUsingSettings ();
+			} else if (RoomName.text.Length == 0 && (Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork || Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork))  {
+				StartCoroutine (RoomNameWarning("PLEASE ENTER THE ROOM NAME"));
+			}
+		}
+
+		public IEnumerator RoomNameWarning(string warn)
+		{
+			WarningText.text = warn;
+			yield return new WaitForSeconds (1);
+			WarningText.text = null;
+		}
+
+		private void FinishSharing(eShareResult _result)
+		{
+			print (_result);
+		}
+
+		public override void OnConnectedToMaster()
+		{
+			print ("Conneced to master server:");
+			PhotonNetwork.JoinLobby ();
+		}
+		public override void OnJoinedLobby()
+		{
+			print ("Joined lobby");
+			if (PlayerLength == 2) {
+				PhotonNetwork.JoinOrCreateRoom (RoomName.text, new Photon.Realtime.RoomOptions {
+					MaxPlayers = 2,
+					PlayerTtl = 300000,
+					EmptyRoomTtl = 10000
+				}, null);
+			} else if (PlayerLength == 4) {
+				PhotonNetwork.JoinOrCreateRoom (RoomName.text, new Photon.Realtime.RoomOptions {
+					MaxPlayers = 4,
+					PlayerTtl = 300000,
+					EmptyRoomTtl = 10000
+				}, null);
+			}
+		}
+		public override void OnCreatedRoom()
+		{
+			print ("Room Created Successfully");
+			ShareSheet shareSheet = new ShareSheet ();
+			shareSheet.Text = RoomName.text;
+			NPBinding.Sharing.ShowView (shareSheet, FinishSharing);
+			isMaster = true;
+		}
+		public override void OnCreateRoomFailed(short msg,string msg1)
+		{
+			print(msg1);
+
+			PhotonNetwork.JoinRoom ("nsd");
+		}
+		public override void OnJoinedRoom()
+		{
+			print (PhotonNetwork.MasterClient.NickName);
+			print ("Room Joined successfully");
+			if (PhotonNetwork.PlayerList.Length == 2) 
+			{
+				isRemote = true;
+			}
+			if (!JoinedRoomFlag) {
+//				SceneManager.LoadScene ("OneOnOneGameBoard");
+				PlayMode=1;
+				SceneManager.LoadScene("BettingAmountFor2PlayerPlayWithFriends");
+			}
+			JoinedRoomFlag = true;
+		}
+		void OnApplicationQuit()
+		{
+			if (Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork || Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork) 
+			{
+				print ("Quit the Application When internet connection is There and Logged out");
+				StartCoroutine (HitUrl11 (0));
+				PlayerPrefs.SetString ("userid", null);
+			}
+			else if(Application.internetReachability == NetworkReachability.NotReachable)
+			{
+				print ("no Internet connection is there");
+			}
+		}
+		IEnumerator HitUrl11(int status)
+		{
+			print ("HitUrl11");
+			string id= PlayerPrefs.GetString ("userid");
+			UnityWebRequest request11 =new UnityWebRequest("http://apienjoybtc.exioms.me/api/Home/login_session?userid="+id+"&gamesessionid="+status);
+
+			request11.chunkedTransfer = false;
+
+			request11.downloadHandler = new DownloadHandlerBuffer ();
+
+			yield return request11.SendWebRequest ();
+
+			if (request11.error != null) {
+
+			} else {
+				print (request11.downloadHandler.text);
+			}
+		}
+	}
+}
